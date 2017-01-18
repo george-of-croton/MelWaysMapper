487eeedf06fadbf0274d1319cc9218c1df8e7270
george-of-croton <armstronggeorge10@gmail.com>
2017-01-18 17:08:10 +1100

    aws S3 integration baby!

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var webshot = require('webshot')
-var fs = require('fs')
+var Readable = require('stream').Readable;
+var aws = require('aws-sdk')
 var url = 'http://localhost:3000/coords/'
+var dotenv = require('dotenv').config()
+
+aws.config = {"accessKeyId": process.env.AWS_ACCESS_KEY_ID, "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY, "region": "us-west-1"}
 
 
 // * GET home page. */
@@ -13,8 +17,18 @@ router.get('/', function(req, res, next) {
 
 router.get('/first/:lat/:lng', function (req, res, next) {
   console.log("hello")
-  webshot('google.com', './boom!.png', function (err){
+  webshot('google.com', function (err, stream){
     if(err) console.log(err)
+
+    var s3 = new aws.S3( {params: {Bucket:'elasticbeanstalk-us-west-1-281842912445', Key: 'google.png'}})
+
+    var readableStream = new Readable().wrap(stream);
+
+    s3.upload({Body: readableStream}, function(err, data) {
+      if(err) return console.log(err);
+
+      console.log(data)
+    })
     console.log("weehee!")
   })
   res.send("this is what happens")
