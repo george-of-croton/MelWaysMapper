f715dc7d41cb8090c645c9adb63146e6e61bd003
george-of-croton <armstronggeorge10@gmail.com>
2017-01-19 10:21:24 +1100

    make it so

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,7 +7,7 @@ document.onload = function() {
 
 function centreMapOnAddress(lat, lon) { //this function just follows the steps on http://www.street-directory.com.au/sd3/mapAPI/index.php
 	var myMap = new JMap(document.getElementById("map-canvas"));
-	var myLonLat = new JLonLat(lon + .0035, lat - .0025);
+	var myLonLat = new JLonLat(lon - .0035, lat - .0025);
 	var point = new JLonLat(lon, lat);
 	var icon = new JIcon(J_DEFAULT_ICON);
 	icon.iconSize = {
