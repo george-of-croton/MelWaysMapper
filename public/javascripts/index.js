f8acd22d834f709878fd4ed2a8f7e1fcc3285887
george-of-croton <armstronggeorge10@gmail.com>
2017-01-20 14:43:23 +1100

    make it so

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,13 @@
 // var request = require('request')
 // var dotenv = require('dotenv')
-document.onload = function() {
+window.onload = function() {
 	var lon = document.getElementById('lon')
 	var lat = document.getElementById('lat')
 
-	centreMapOnAddress(lat, lon)
 }
 var mappy;
 
-function centreMapOnAddress(lat, lon, level) { //this function just follows the steps on http://www.street-directory.com.au/sd3/mapAPI/index.php
+var centreMapOnAddress = function(lat, lon, level) { //this function just follows the steps on http://www.street-directory.com.au/sd3/mapAPI/index.php
 	var myMap = new JMap(document.getElementById("map-canvas"));
 	var myLonLat = new JLonLat(lon, lat - .0010);
 	var point = new JLonLat(lon, lat);
@@ -44,19 +43,20 @@ function grabMapAsIs(lat, lon) {
 		zoom: mappy.getBoundsZoomLevel(mappy.getBounds())
 	}
 
-	// 	request.get(url, function(req, res, next) {
-	// 		console.log(res.body, "response body")
-	// 		console.log(typeof(res.body))
-	// 		var img = document.createElement("img")
-	// 		img.src = res.body
-	// 		document.body.appendChild(img)
-	// 	})
-	//
-	// 	var url = function() {
-	// 		return process.env.REQUESTURLBASE + "/" + lat + '/' + lon + '/' + mapState.zoom + "/" + mapState.centre.lat + "/" + mapState.centre.lon
-	// 	}
-	//
-	// }
+	request.get(url, function(req, res, next) {
+		console.log(res.body, "response body")
+		console.log(typeof(res.body))
+		var img = document.createElement("img")
+		img.src = res.body
+		document.body.appendChild(img)
+	})
 
-	//
-	// }
+	var url = function() {
+		return process.env.REQUESTURLBASE + "/" + lat + '/' + lon + '/' + mapState.zoom + "/" + mapState.centre.lat + "/" + mapState.centre.lon
+	}
+
+}
+
+
+}
+}
