26b01e3e05c72d1ffdb89feef67f6e9e2285d24f
george-of-croton <armstronggeorge10@gmail.com>
2017-01-20 15:37:28 +1100

    make it so

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ var start;
 
 aws.config = {
 	"accessKeyId": process.env.AWS_ACCESS_KEY_ID,
-	"secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY
+	"secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+	"region": "us-east-1"
 }
 
 var options = {
