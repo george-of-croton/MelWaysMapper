25b4f449c083f49ad94eb448a489369774c85f1a
george-of-croton <armstronggeorge10@gmail.com>
2017-01-20 15:07:19 +1100

    make it happen

--- tests/requestTest.js
+++ tests/requestTest.js
@@ -1,5 +1,5 @@
 var request = require('request')
 
-request.get('https://mysterious-taiga-89115.herokuapp.com/coords/first/-37.829887/144.980803/9', function(req, res) {
+request.get('https://mysterious-taiga-89115.herokuapp.com/coords/first/-37.829887/144.980803/13', function(req, res) {
 	console.log(res.body)
 })
