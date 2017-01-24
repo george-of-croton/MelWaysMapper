e679317fd7b919632ce801ac77d96ee617855008
george-of-croton <armstronggeorge10@gmail.com>
2017-01-25 10:01:55 +1100

    add basic tape test

--- tests/requestTest.js
+++ tests/requestTest.js
@@ -1,6 +1,18 @@
 var request = require('request')
 var fs = require('fs')
+var url = 'http://node-express-env.xdccgnj5d2.ap-southeast-2.elasticbeanstalk.com/coords/first/-37.83787/144.980803/13/-37.83787/144.980803'
+var test = require('tape')
 
-request.get({
-	url: 'http://node-express-env.xdccgnj5d2.ap-southeast-2.elasticbeanstalk.com/coords/first/-37.83787/144.980803/13',
-}).pipe(fs.createWriteStream('./test.jpeg'))
+
+test('file test',
+	function(t) {
+		t.plan(2)
+		t.equal(typeof url, "string")
+		fs.readFile('./tests/test.png', function read(err, data) {
+			if (err) {
+				console.log(err);
+			}
+			console.log(data)
+			t.equal(typeof data, "object")
+		})
+	})
